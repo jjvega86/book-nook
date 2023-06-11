cf1a4cba6b3a726245b0c378568b2f5a54354ab7
JJ <jj.vega86@gmail.com>
2023-06-11 17:01:43 -0400

    📦 NEW: Add render books to main books page

--- app/routes/books.tsx
+++ app/routes/books.tsx
@@ -1,9 +1,10 @@
 import { V2_MetaFunction, json } from "@remix-run/node";
 import type { LoaderArgs } from "@remix-run/node";
-import { Link, useLoaderData } from "@remix-run/react";
+import { useLoaderData } from "@remix-run/react";
 import authenticator from "~/services/auth.server";
 
 import NavBar from "~/components/NavBar";
+import { getBooks } from "~/services/books.server";
 
 export const meta: V2_MetaFunction = () => {
   return [
@@ -12,27 +13,36 @@ export const meta: V2_MetaFunction = () => {
   ];
 };
 
+type Book = {
+  volumeInfo: {
+    title: string;
+  };
+};
+
 export const loader = async ({ request }: LoaderArgs) => {
   let user = await authenticator.isAuthenticated(request);
 
-  return json(user);
+  let books = await getBooks("harry potter");
+  let prunedBooks = books.slice(0, 10);
+
+  return json({ user, books: prunedBooks });
 };
 
 const Books = () => {
-  const user = useLoaderData();
+  const { user, books } = useLoaderData();
+  // TODO: Fetch default book search and render
   // TODO: search for books (use the "useSubmit" hook to submit on user input, see Remix docs)
+  // TODO: Add pagination for all book results (start pruned)
   return (
     <div>
       <header>
         <NavBar user={user} />
       </header>
       <main>
-        <h1>Welcome to Book Nook</h1>
-        {user ? (
-          <pre>userId: {user.userId}</pre>
-        ) : (
-          <Link to="/login">Login Here</Link>
-        )}
+        {books &&
+          books.map((book: Book) => (
+            <p key={book.volumeInfo.title}>{book.volumeInfo?.title}</p>
+          ))}
       </main>
     </div>
   );
--- app/services/books.server.ts
+++ app/services/books.server.ts
@@ -1,1 +1,14 @@
 // TODO: Add functions for Google Books requests
+
+export const getBooks = async (searchTerm: string) => {
+  try {
+    let response = await fetch(
+      `https://www.googleapis.com/books/v1/volumes?q=${searchTerm}`
+    );
+
+    let data = await response.json();
+    return data.items;
+  } catch (error) {
+    console.error(error);
+  }
+};
